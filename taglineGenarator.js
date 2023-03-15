403478f9601788175571270a91dd99953babd826
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-15 07:02:30 +0600

    show randowm button bug fixed and random data showing done

--- taglineGenarator.js
+++ taglineGenarator.js
@@ -21,27 +21,26 @@ function generateTag(arr, interval) {
 
 generateTag(tagLines, 3000);
 
-document
-  .getElementById("random-room-btn")
-  .addEventListener("click", async function () {
+document.getElementById("random-room-btn").addEventListener("click", async function () {
     console.log("hello world");
     const data = await fetch(`ROOMS.json`);
     const result = await data.json();
+    console.log(result);
     const randomIndex = Math.floor(Math.random() * 10);
     const modalBody = document.getElementById("random-room-info-modal-body");
-    const { name, summary, property_type, images, review_scores } = result[9];
+    const { name, summary, property_type, images, review_scores } = result[randomIndex];
     
     
 
     const reviews = document.createElement("ol");
-    if (review_scores.scores) {
+    if (review_scores?.scores) {
       reviews.setAttribute('class',"list-group")
 
-      object.keys(review_scores.scores).forEach((key) => {
+      Object.keys(review_scores?.scores).forEach((key) => {
         reviews.innerHTML += `
         <li class="list-group-item list-group-item-info d-flex justify-content-between align-items-center">
         ${key} : 
-        <span class="badge bg-primary rounded-pill">${review_scores.scores[key]}</span> 
+        <span class="badge bg-primary rounded-pill">${review_scores?.scores[key]}</span> 
         </li>
         `;
       });
@@ -61,7 +60,7 @@ document
         <p class="card-text">${summary}</p>
         <div id='review-score'>
       Review Scores :
-      ${review_scores.scores.review_scores_accuracy}
+      ${review_scores?.scores?.review_scores_accuracy}
     </div>
       </div>
       <button class="btn btn-info btn-lg"  role="button"
